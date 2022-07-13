85b114b62c3f22cf43935625478193d38d4c327e
Sunil Kumar Majhi <msunil037@gmail.com>
2022-07-13 16:25:52 +0530

    prime number programs

--- prime.cpp
+++ prime.cpp
@@ -2,21 +2,24 @@
 *   Find the prime number between two number
 */
 #include <iostream>
+#include <math.h>
 using namespace std;
-
+bool isPrime(int num){
+        for(int i = 2; i <= sqrt(num); i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+    return true;
+}
 int main() {
     int a,b;
     cin>>a>>b;
 
     for(int num = a; num<=b; num++){
-        int i;
-        for(i = 2; i < num; i++){
-            if(num%i == 0){
-             break;
-            }
-        }
-        if(i == num){
+        if(isPrime(num)){
             cout<<num<<endl;
         }
+
     }
 }
